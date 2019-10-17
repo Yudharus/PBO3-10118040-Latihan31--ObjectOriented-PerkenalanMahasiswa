86b12fe426dd34ebc275d1b27d6d3fb5a119b66f
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-10-17 14:41:04 +0700

    Add files via upload

---
+++ PBO3-10118040-Latihan31-(ObjectOriented)PerkenalanMahasiswa/src/pbo3/pkg10118040/latihan31/objectoriented/perkenalanmahasiswa/PBO310118040Latihan31ObjectOrientedPerkenalanMahasiswa.java
@@ -0,0 +1,47 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10118040.latihan31.objectoriented.perkenalanmahasiswa;
+
+/**
+ *
+ * @author asus
+ *Nama              :Moch Yudha Rusdian
+ * NIM              : 10118040
+ * KELAS            :IF-1
+ * MATERI:          :Perkenalan Mahasiswa
+ */
+public class PBO310118040Latihan31ObjectOrientedPerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+        mahasiswa mahasiswa = new mahasiswa();
+           
+            
+            
+            mahasiswa. nim = "10118040";
+            mahasiswa.nama ="Moch Yudha Rusdian";
+            mahasiswa.perkenalanDiri();
+            
+            mahasiswa.nim = "10118031";
+            mahasiswa.nama = "Rizky Aria";
+            mahasiswa.perkenalanDiri();
+            
+            mahasiswa.nim = "10118032";
+            mahasiswa.nama = "Guntur Maulana I";
+            mahasiswa.perkenalanDiri();
+            
+            mahasiswa.nim = "10118001";
+            mahasiswa.nama = "Alfian Soasiu";
+            mahasiswa.perkenalanDiri();
+    }
+    
+    
+}
---
+++ PBO3-10118040-Latihan31-(ObjectOriented)PerkenalanMahasiswa/src/pbo3/pkg10118040/latihan31/objectoriented/perkenalanmahasiswa/mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10118040.latihan31.objectoriented.perkenalanmahasiswa;
+
+/**
+ *
+ * @author asus
+ */
+public class mahasiswa {
+   public String nim;
+    public String nama;
+
+	public void perkenalanDiri() {
+	      
+            System.out.println("Hallo Everyone");
+            System.out.println("My NIM is " + nim);
+            System.out.println("My name is " + nama);
+            System.out.println();
+	} 
+}
